587ff057e925d066686077a9e838f7d17b8b78df
MugenAttack <MugenAttack@ymail.com>
2015-07-26 23:21:55 -0500

    Program uploaded

---
+++ agdDecryptor/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace agdDecryptor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toXml = new System.Windows.Forms.Button();
+            this.toAgd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // toXml
+            // 
+            this.toXml.Location = new System.Drawing.Point(8, 12);
+            this.toXml.Name = "toXml";
+            this.toXml.Size = new System.Drawing.Size(141, 53);
+            this.toXml.TabIndex = 0;
+            this.toXml.Text = "agd to xml";
+            this.toXml.UseVisualStyleBackColor = true;
+            this.toXml.Click += new System.EventHandler(this.toXml_Click);
+            // 
+            // toAgd
+            // 
+            this.toAgd.Location = new System.Drawing.Point(155, 12);
+            this.toAgd.Name = "toAgd";
+            this.toAgd.Size = new System.Drawing.Size(141, 53);
+            this.toAgd.TabIndex = 1;
+            this.toAgd.Text = "xml to agd";
+            this.toAgd.UseVisualStyleBackColor = true;
+            this.toAgd.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 74);
+            this.Controls.Add(this.toAgd);
+            this.Controls.Add(this.toXml);
+            this.Name = "Form1";
+            this.Text = "Avatar Growth Data XML Tool";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button toXml;
+        private System.Windows.Forms.Button toAgd;
+    }
+}
+
---
+++ agdDecryptor/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace agdDecryptor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //convert xml to agd
+            List<byte> filebyte = new List<byte>();
+            filebyte.Add(0x23);
+            filebyte.Add(0x41);
+            filebyte.Add(0x47);
+            filebyte.Add(0x44);
+            filebyte.Add(0xFE);
+            filebyte.Add(0xFF);
+            filebyte.Add(0x10);
+            filebyte.Add(0x00);
+
+            XmlDocument xd = new XmlDocument();
+            xd.Load("avatar_growth_data.xml");
+            XmlNodeList levels = xd.SelectSingleNode("Data").ChildNodes;
+            byte[] count = BitConverter.GetBytes(levels.Count);
+            filebyte.Add(count[0]);
+            filebyte.Add(count[1]);
+            filebyte.Add(count[2]);
+            filebyte.Add(count[3]);
+
+            filebyte.Add(0x10);
+            filebyte.Add(0x00);
+            filebyte.Add(0x00);
+            filebyte.Add(0x00);
+            int value;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                value = int.Parse(levels.Item(i).Attributes["ID"].Value);
+                count = BitConverter.GetBytes(value);
+                filebyte.Add(count[0]);
+                filebyte.Add(count[1]);
+                filebyte.Add(count[2]);
+                filebyte.Add(count[3]);
+
+                value = int.Parse(levels.Item(i).Attributes["ExptoLevel"].Value);
+                count = BitConverter.GetBytes(value);
+                filebyte.Add(count[0]);
+                filebyte.Add(count[1]);
+                filebyte.Add(count[2]);
+                filebyte.Add(count[3]);
+
+                value = int.Parse(levels.Item(i).Attributes["RequiredExp"].Value);
+                count = BitConverter.GetBytes(value);
+                filebyte.Add(count[0]);
+                filebyte.Add(count[1]);
+                filebyte.Add(count[2]);
+                filebyte.Add(count[3]);
+
+                value = int.Parse(levels.Item(i).Attributes["AttributePoints"].Value);
+                count = BitConverter.GetBytes(value);
+                filebyte.Add(count[0]);
+                filebyte.Add(count[1]);
+                filebyte.Add(count[2]);
+                filebyte.Add(count[3]);
+
+
+            }
+
+
+            FileStream fs = new FileStream("avatar_growth_data.agd", FileMode.Create);
+            fs.Write(filebyte.ToArray(), 0, filebyte.Count);
+            fs.Close();
+        }
+
+        private void toXml_Click(object sender, EventArgs e)
+        {
+            //convert agd to xml
+            XmlWriter xw = XmlWriter.Create("avatar_growth_data.xml");
+            byte[] readByte = File.ReadAllBytes("avatar_growth_data.agd");
+            byte[] readFour = new byte[4];
+            ReadFourBytes(ref readByte,ref readFour, 8);
+            int LastID = BitConverter.ToInt32(readFour, 0);
+            //MessageBox.Show(LastID.ToString());
+            ReadFourBytes(ref readByte, ref readFour, 12);
+            int pos = BitConverter.ToInt32(readFour, 0);
+            //MessageBox.Show(pos.ToString());
+            int ID = 0;
+            int Value;
+            xw.WriteStartDocument();
+            xw.WriteStartElement("Data");
+            do
+            {
+                xw.WriteStartElement("level");
+                ReadFourBytes(ref readByte, ref readFour, pos);
+                ID = BitConverter.ToInt32(readFour, 0);
+                xw.WriteAttributeString("ID", Convert.ToString(ID));
+                pos += 4;
+
+                ReadFourBytes(ref readByte, ref readFour, pos);
+                Value = BitConverter.ToInt32(readFour, 0);
+                xw.WriteAttributeString("ExptoLevel", Convert.ToString(Value));
+                pos += 4;
+
+                ReadFourBytes(ref readByte, ref readFour, pos);
+                Value = BitConverter.ToInt32(readFour, 0);
+                xw.WriteAttributeString("RequiredExp", Convert.ToString(Value));
+                pos+=4;
+
+                ReadFourBytes(ref readByte, ref readFour, pos);
+                Value = BitConverter.ToInt32(readFour, 0);
+                xw.WriteAttributeString("AttributePoints", Convert.ToString(Value));
+                pos += 4;
+                xw.WriteEndElement();
+           
+            } while (ID != LastID);
+            xw.WriteEndElement();
+            xw.WriteEndDocument();
+            xw.Close();
+    
+
+        }
+
+        public void ReadFourBytes(ref byte[] Allbytes,ref byte[] Fourbytes, int pos)
+        {
+            for (int j= 0; j < 4; j++)
+            {
+                Fourbytes[j] = Allbytes[pos + j];
+            }
+        }
+    }
+}
